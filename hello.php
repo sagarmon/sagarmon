d2d4afb8bdb69004ebbebe05a196b191504db9e6
Sagar Sarkar <sagar_mon@hotmail.com>
2020-12-22 16:29:03 +0600

    Update hello.php

--- hello.php
+++ hello.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<?php
 <html>
 <head>
 	<title>Sagar Sarkar</title>
@@ -13,3 +13,4 @@
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script> -->
 </body>
 </html>
+?>
